ae551ba9e529451e7ec533189ea07cf0463473a3
Kevin <qiwen@qiwen.name>
2020-03-12 22:57:53 +0800

    update configs

--- scripts/finetune_tsm_ucf101_rgb_8f.sh
+++ scripts/finetune_tsm_ucf101_rgb_8f.sh
@@ -3,7 +3,8 @@ python main.py ucf101 RGB \
      --gd 20 --lr 0.001 --lr_steps 10 20 --epochs 25 \
      --batch-size 32 -j 16 --dropout 0.8 --consensus_type=avg --eval-freq=1 \
      --shift --shift_div=8 --shift_place=blockres \
-     --tune_from=pretrained/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth
+     --tune_from=pretrained/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e100_dense_nl.pth --non_local
+     #--tune_from=pretrained/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth
 
 # https://hanlab.mit.edu/projects/tsm/models/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth
 # https://hanlab.mit.edu/projects/tsm/models/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e100_dense.pth
